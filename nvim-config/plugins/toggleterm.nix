d98b29c8251f72f2abdfa367497dbb379584e3a5
RTChou67 <3023207450@tju.edu.cn>
2025-10-28 05:38:27 +0000

    dev

--- nvim-config/plugins/toggleterm.nix
+++ nvim-config/plugins/toggleterm.nix
@@ -1,16 +1,28 @@
 { config, pkgs, ... }:
 {
-  programs.nixvim.plugins.toggleterm = {
-    enable = true;
-
-    settings = {
-      size = 20;
-      direction = "float";
-      start_in_insert = true;
-      close_on_exit = true;
-      float_opts = {
-        border = "rounded";
+  programs.nixvim = {
+    plugins.toggleterm = {
+      enable = true;
+      settings = {
+        size = 10;
+        direction = "horizontal";
+        start_in_insert = true;
+        close_on_exit = true;
+        float_opts = {
+          border = "rounded";
+        };
       };
     };
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>t";
+        action = "<cmd>ToggleTerm direction=horizontal<CR>";
+        options = {
+          desc = "ToggleTerm (Horizontal)";
+          silent = true;
+        };
+      }
+    ];
   };
 }
--- nvim-config/plugins/treesitter.nix
+++ nvim-config/plugins/treesitter.nix
@@ -5,6 +5,21 @@
     settings = {
       indent.enable = true;
       highlight.enable = true;
+      textobjects = {
+        enable = true;
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            "ic" = "@class.inner";
+            "ia" = "@parameter.inner";
+            "aa" = "@parameter.outer";
+          };
+        };
+      };
     };
     folding = false;
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
