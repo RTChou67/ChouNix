613727eca6f394eae517e0991ca0a9fdf3db5fc8
RTChou67 <3023207450@tju.edu.cn>
2025-11-01 17:30:51 +0000

    dev

--- configuration.nix
+++ configuration.nix
@@ -84,8 +84,11 @@
     yarn
     zip
     zlib-ng
+  ];
+  environment.shells = with pkgs; [
     zsh
-
+    nushell
+    elvish
   ];
   programs.direnv = {
     enable = true;
@@ -151,5 +154,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
